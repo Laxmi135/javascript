6c007c8ecc47c642514eca7863660ca49c0624c2
Laxmi135 <laxmisharma6356@gmail.com>
2025-08-18 08:27:30 +0000

    done

---
+++ num.js
@@ -0,0 +1,20 @@
+const score = 100;
+// console.log(score);
+const balance = new Number(100);
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed(1));
+const other = 123.00
+// console.log(other.toPrecision(3));
+const hundred = 100000;
+// console.log(hundred.toLocaleString());
+
+console.log(Math);
+console.log(Math.sqrt(4));
+
+
+
+
+
+
+
