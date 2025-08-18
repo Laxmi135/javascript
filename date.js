80d47d9576d707e4c7f406872d89149b59afe918
Laxmi135 <laxmisharma6356@gmail.com>
2025-08-18 10:34:44 +0000

    done

---
+++ date.js
@@ -0,0 +1,18 @@
+const mydate = new Date()
+console.log(mydate);
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toString());
+
+// const otherdate = new  Date(2001,10,27)
+// console.log(otherdate.toDateString());
+
+const realdate = new  Date(2001,10,27,3,5)
+console.log(realdate.toLocaleDateString());
+console.log(realdate.toLocaleString());
+
+
+
+
+
+
