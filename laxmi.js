d42893f920e177c5eccd14e2bceda6721520f429
Laxmi135 <laxmisharma6356@gmail.com>
2025-08-18 05:11:31 +0000

    done

--- laxmi.js
+++ laxmi.js
@@ -4,6 +4,18 @@ console.log(name + age);
 console.log(name.toUpperCase());
 console.log(name.indexOf('x'));
 console.log(name.charAt(3));
+var newname = name.substring(0,4)
+console.log(newname);
+var another = name.slice(-8,3)
+console.log(another);
+var newone = " laxmi "
+console.log(newone);
+console.log(newone.trim());
+
+
+
+
+
 
 
 
